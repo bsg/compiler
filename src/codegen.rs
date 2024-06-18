529082cd36219bb7bf27432d81ac33fa2b663b17
bsg <cem.saldirim@gmail.com>
2024-06-19 01:48:45 +0300

    woop

--- src/codegen.rs
+++ src/codegen.rs
@@ -1,7 +1,5 @@
-use core::ffi::CStr;
 use llvm_sys::core::*;
 use llvm_sys::prelude::*;
-use llvm_sys::LLVMValue;
 use std::collections::HashMap;
 use std::ffi::CString;
 
@@ -23,18 +21,42 @@ impl ToCStr for String {
     }
 }
 
-pub struct Module {
+pub struct Var {
+    val: LLVMValueRef,
+    ty: LLVMTypeRef,
+}
+
+pub struct Env {
+    vars: HashMap<String, Var>,
+}
+
+impl Env {
+    pub fn new() -> Self {
+        Env {
+            vars: HashMap::new(),
+        }
+    }
+
+    pub fn insert_var(&mut self, ident: String, val: LLVMValueRef, ty: LLVMTypeRef) {
+        self.vars.insert(ident, Var { val, ty });
+    }
+
+    pub fn get_var(&mut self, ident: &String) -> Option<&Var> {
+        self.vars.get(ident)
+    }
+}
+
+pub struct ModuleBuilder {
     name: String,
     module: LLVMModuleRef,
     builder: LLVMBuilderRef,
     types: HashMap<String, LLVMTypeRef>,
+    env: Env,
 }
 
-impl Module {
+impl ModuleBuilder {
     pub fn new(name: &str) -> Self {
-        let mod_name = CStr::from_bytes_with_nul(format!("{}\0", name).as_bytes())
-            .unwrap()
-            .as_ptr();
+        let mod_name = name.to_cstring().as_ptr();
         let module = unsafe { LLVMModuleCreateWithName(mod_name) };
         let builder = unsafe { LLVMCreateBuilder() };
 
@@ -48,6 +70,7 @@ impl Module {
             module,
             builder,
             types,
+            env: Env::new(),
         }
     }
 
@@ -55,22 +78,72 @@ impl Module {
         match &*node {
             Node::Int(i) => {
                 // FIXME int type
+                // TODO sign extend
                 unsafe { LLVMConstInt(LLVMInt32Type(), i.value.try_into().unwrap(), 0) }
+            }
+            _ => unimplemented!(),
+        }
+    }
+
+    fn build_binop(&mut self, node: BinOpNode) -> LLVMValueRef {
+        match node.op {
+            Op::Add => unsafe {
+                LLVMBuildAdd(
+                    self.builder,
+                    self.build_expr(node.lhs),
+                    self.build_expr(node.rhs),
+                    "".to_cstring().as_ptr(),
+                )
             },
-            _ => unimplemented!()
+            _ => unimplemented!(),
+        }
+    }
+
+    fn build_expr(&mut self, node: NodeRef) -> LLVMValueRef {
+        match &*node {
+            Node::Int(_) => self.build_value(node),
+            Node::Ident(ident) => unsafe {
+                let var = self.env.get_var(&ident.name.to_string()).unwrap();
+                LLVMBuildLoad2(self.builder, var.ty, var.val, "".to_cstring().as_ptr())
+            },
+            Node::BinOp(op) => self.build_binop(op.clone()),
+            _ => unimplemented!(),
+        }
+    }
+
+    fn build_stmt(&mut self, node: NodeRef) -> LLVMValueRef {
+        match &*node {
+            Node::Return(r) => {
+                if let Some(rhs) = &r.stmt {
+                    let v = self.build_expr(rhs.clone());
+                    unsafe { LLVMBuildRet(self.builder, v) }
+                } else {
+                    unsafe { LLVMBuildRetVoid(self.builder) }
+                }
+            }
+            Node::Let(l) => unsafe {
+                if let Node::Ident(ident) = &*l.lhs {
+                    let reg = LLVMBuildAlloca(
+                        self.builder,
+                        *self.types.get(&*l.ty).unwrap(),
+                        "".to_cstring().as_ptr(),
+                    );
+                    let rhs = self.build_expr(l.rhs.clone());
+                    let ty = self.types.get(&*l.ty).unwrap();
+                    self.env.insert_var(ident.name.to_string(), reg, *ty);
+                    LLVMBuildStore(self.builder, rhs, reg)
+                } else {
+                    panic!()
+                }
+            },
+            _ => unimplemented!(),
         }
     }
 
     fn build_block(&mut self, node: NodeRef) {
         if let Node::Block(block) = &*node {
             for stmt in block.statements.iter() {
-                match &**stmt {
-                    Node::Return(r) => {
-                        let v = self.build_value(r.stmt.clone().unwrap());
-                        unsafe { LLVMBuildRet(self.builder, v) };
-                    },
-                    _ => unimplemented!()
-                };
+                self.build_stmt(stmt.clone());
             }
         } else {
             panic!()
@@ -79,7 +152,7 @@ impl Module {
 
     pub fn build_func(&mut self, node: NodeRef) {
         if let Node::Fn(func) = &*node {
-            let fn_type = self.types.get("void").unwrap();
+            let fn_type = self.types.get(&*func.ret_ty).unwrap();
             let function_type = unsafe { LLVMFunctionType(*fn_type, [].as_mut_ptr(), 0, 0) };
             let function = unsafe {
                 LLVMAddFunction(self.module, func.ident.to_cstring().as_ptr(), function_type)
@@ -96,7 +169,7 @@ impl Module {
     }
 }
 
-impl Drop for Module {
+impl Drop for ModuleBuilder {
     fn drop(&mut self) {
         unsafe { LLVMDisposeBuilder(self.builder) };
     }
