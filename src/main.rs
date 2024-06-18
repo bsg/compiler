eb6a3a5faaa008e9d94255b8eedcb35a5c9940df
bsg <cem.saldirim@gmail.com>
2024-06-18 05:43:35 +0300

    eeeh

--- src/main.rs
+++ src/main.rs
@@ -63,6 +63,7 @@ impl Ctx {
 impl Default for Ctx {
     fn default() -> Self {
         let mut types = HashMap::new();
+        types.insert("void".to_string(), Type::new("void", LlvmTy::Void, 1));
         types.insert("bool".to_string(), Type::new("bool", LlvmTy::I1, 1));
         types.insert("i32".to_string(), Type::new("i32", LlvmTy::I32, 4));
         types.insert("u32".to_string(), Type::new("u32", LlvmTy::I32, 4));
@@ -119,31 +120,70 @@ fn emit_fn(ctx: &mut Ctx, node: NodeRef) -> String {
             args.pop();
             fn_ctx.next_reg();
             let body = emit_block(ctx, &mut fn_ctx, body.clone());
-            format!("define {} @{ident}({args}) {{{body}\n}}", ret_type.as_str())
+            format!(
+                "define {} @{ident}({args}) {{{}\n}}",
+                ret_type.as_str(),
+                body.code
+            )
         }
         _ => panic!(),
     }
 }
 
-fn emit_block(ctx: &mut Ctx, fn_ctx: &mut FnCtx, node: NodeRef) -> String {
+struct IrBlock {
+    code: String,
+    is_terminal: bool,
+}
+
+fn emit_block(ctx: &mut Ctx, fn_ctx: &mut FnCtx, node: NodeRef) -> IrBlock {
     let mut body = String::new();
+    let mut is_terminal = false;
 
     if let NodeKind::Block(block) = &node.kind {
         for stmt in block.statements.iter() {
+            let stmt = emit_stmt(ctx, fn_ctx, stmt.clone());
+            if stmt.is_terminal {
+                if !is_terminal {
+                    is_terminal = true;
+                } else {
+                    // TODO stmt is unreachable
+                    todo!()
+                }
+            }
             body += "\n";
-            body += &emit_stmt(ctx, fn_ctx, stmt.clone());
+            body += &stmt.code;
         }
-        body
+        IrBlock {
+            code: body,
+            is_terminal,
+        }
     } else {
         panic!()
     }
 }
 
-fn emit_stmt(ctx: &mut Ctx, fn_ctx: &mut FnCtx, node: NodeRef) -> String {
+struct IrStmt {
+    code: String,
+    is_terminal: bool,
+}
+
+fn emit_stmt(ctx: &mut Ctx, fn_ctx: &mut FnCtx, node: NodeRef) -> IrStmt {
     match &node.kind {
-        NodeKind::Return => match emit_expr(ctx, fn_ctx, node.right.clone().unwrap()) {
-            EmitExprRes::Imm(imm) => format!("ret {} {}", imm.ty.llvm_ty.as_str(), imm.code),
-            EmitExprRes::Reg(r) => format!("{}ret {} %{}", r.code, r.ty.llvm_ty.as_str(), r.reg),
+        NodeKind::Return => match &node.right {
+            Some(rhs) => match emit_expr(ctx, fn_ctx, rhs.clone()) {
+                IrExpr::Imm(imm) => IrStmt {
+                    code: format!("ret {} {}", imm.ty.llvm_ty.as_str(), imm.code),
+                    is_terminal: true,
+                },
+                IrExpr::Reg(r) => IrStmt {
+                    code: format!("{}ret {} %{}", r.code, r.ty.llvm_ty.as_str(), r.reg),
+                    is_terminal: true,
+                },
+            },
+            None => IrStmt {
+                code: "ret void".to_owned(),
+                is_terminal: true,
+            },
         },
         NodeKind::Let(let_stmt) => {
             let reg = fn_ctx.next_reg();
@@ -157,47 +197,76 @@ fn emit_stmt(ctx: &mut Ctx, fn_ctx: &mut FnCtx, node: NodeRef) -> String {
                     },
                 );
                 match emit_expr(ctx, fn_ctx, node.right.clone().unwrap()) {
-                    EmitExprRes::Imm(imm) => format!(
-                        "%{reg} = alloca {}, align {}\nstore {} {}, ptr %{reg}, align {}",
-                        imm.ty.llvm_ty.as_str(),
-                        imm.ty.align,
-                        imm.ty.llvm_ty.as_str(),
-                        imm.code,
-                        imm.ty.align,
-                    ),
-                    EmitExprRes::Reg(r) => format!(
-                        "%{reg} = alloca {}, align {}\n{}store {} %{}, ptr %{reg}, align {}",
-                        r.ty.llvm_ty.as_str(),
-                        r.ty.align,
-                        r.code,
-                        r.ty.llvm_ty.as_str(),
-                        r.reg,
-                        r.ty.align,
-                    ),
+                    IrExpr::Imm(imm) => IrStmt {
+                        code: format!(
+                            "%{reg} = alloca {}, align {}\nstore {} {}, ptr %{reg}, align {}",
+                            imm.ty.llvm_ty.as_str(),
+                            imm.ty.align,
+                            imm.ty.llvm_ty.as_str(),
+                            imm.code,
+                            imm.ty.align,
+                        ),
+                        is_terminal: false,
+                    },
+                    IrExpr::Reg(r) => IrStmt {
+                        code: format!(
+                            "%{reg} = alloca {}, align {}\n{}store {} %{}, ptr %{reg}, align {}",
+                            r.ty.llvm_ty.as_str(),
+                            r.ty.align,
+                            r.code,
+                            r.ty.llvm_ty.as_str(),
+                            r.reg,
+                            r.ty.align,
+                        ),
+                        is_terminal: false,
+                    },
                 }
             } else {
                 panic!()
             }
         }
         NodeKind::If(if_stmt) => {
-            let true_block = emit_block(ctx, fn_ctx, node.left.clone().unwrap());
-            let false_block = emit_block(ctx, fn_ctx, node.right.clone().unwrap());
+            let mut true_block = emit_block(ctx, fn_ctx, node.left.clone().unwrap());
+            let mut false_block = emit_block(ctx, fn_ctx, node.right.clone().unwrap());
             let l_true = fn_ctx.next_label();
             let l_false = fn_ctx.next_label();
-            let l_merge = fn_ctx.next_label();
-            match emit_expr(ctx, fn_ctx, if_stmt.condition.clone()) {
-                EmitExprRes::Imm(imm) => {
-                    format!(
-                        "br i1 {}, label %l0, label %l1\nl{l_true}:{true_block}\nl{l_false}:{false_block}",
-                        imm.code,
-                    )
+            let mut exit_point: String = String::new();
+
+            if true_block.is_terminal || false_block.is_terminal {
+                let exit_to = fn_ctx.next_label();
+
+                if !true_block.is_terminal {
+                    true_block.code += &format!("\nbr label %l{}", exit_to);
                 }
-                EmitExprRes::Reg(r) => {
-                    format!(
-                        "{}br i1 %{}, label %l0, label %l1\nl{l_true}:{true_block}\nl{l_false}:{false_block}",
+
+                if !false_block.is_terminal {
+                    false_block.code += &format!("\nbr label %l{}", exit_to);
+                }
+
+                exit_point = format!("\nl{}:", exit_to);
+            }
+
+            match emit_expr(ctx, fn_ctx, if_stmt.condition.clone()) {
+                IrExpr::Imm(imm) => IrStmt {
+                    code: format!(
+                        "br i1 {}, label %l{l_true}, label %l{l_false}\nl{l_true}:{}\nl{l_false}:{}{}",
+                        true_block.code,
+                        imm.code,
+                        false_block.code,
+                        exit_point,
+                    ),
+                    is_terminal: true_block.is_terminal && false_block.is_terminal,
+                },
+                IrExpr::Reg(r) => IrStmt {
+                    code: format!(
+                        "{}br i1 %{}, label %l{l_true}, label %l{l_false}\nl{l_true}:{}\nl{l_false}:{}{}",
                         r.code,
                         r.reg,
-                    )
+                        true_block.code,
+                        false_block.code,
+                        exit_point
+                    ),
+                    is_terminal: true_block.is_terminal && false_block.is_terminal,
                 }
             }
         }
@@ -206,25 +275,27 @@ fn emit_stmt(ctx: &mut Ctx, fn_ctx: &mut FnCtx, node: NodeRef) -> String {
                 if let NodeKind::Ident(lhs_ident) = &node.left.clone().unwrap().kind {
                     let lhs = fn_ctx.env.get(&lhs_ident.to_string()).unwrap().reg;
                     match emit_expr(ctx, fn_ctx, node.right.clone().unwrap()) {
-                        EmitExprRes::Imm(imm) => {
-                            format!(
+                        IrExpr::Imm(imm) => IrStmt {
+                            code: format!(
                                 "store {} {}, ptr %{}, align {}",
                                 imm.ty.llvm_ty.as_str(),
                                 imm.code,
                                 lhs,
                                 imm.ty.align
-                            )
-                        }
-                        EmitExprRes::Reg(r) => {
-                            format!(
+                            ),
+                            is_terminal: false,
+                        },
+                        IrExpr::Reg(r) => IrStmt {
+                            code: format!(
                                 "{}store {} %{}, ptr %{}, align {}",
                                 r.code,
                                 r.ty.llvm_ty.as_str(),
                                 r.reg,
                                 lhs,
                                 r.ty.align
-                            )
-                        }
+                            ),
+                            is_terminal: false,
+                        },
                     }
                 } else {
                     panic!()
@@ -232,7 +303,7 @@ fn emit_stmt(ctx: &mut Ctx, fn_ctx: &mut FnCtx, node: NodeRef) -> String {
             }
             _ => unimplemented!(),
         },
-        _ => "".to_string(),
+        _ => unimplemented!(),
     }
 }
 
@@ -247,31 +318,31 @@ struct Reg {
     reg: usize,
 }
 
-enum EmitExprRes {
+enum IrExpr {
     Imm(Imm),
     Reg(Reg),
 }
 
-fn emit_expr(_ctx: &mut Ctx, fn_ctx: &mut FnCtx, node: NodeRef) -> EmitExprRes {
+fn emit_expr(_ctx: &mut Ctx, fn_ctx: &mut FnCtx, node: NodeRef) -> IrExpr {
     match &node.kind {
-        NodeKind::Int(i) => EmitExprRes::Imm(Imm {
+        NodeKind::Int(i) => IrExpr::Imm(Imm {
             ty: Type::new("i32", LlvmTy::I32, 4),
             code: format!("{}", i),
         }),
-        NodeKind::Bool(b) => EmitExprRes::Imm(Imm {
+        NodeKind::Bool(b) => IrExpr::Imm(Imm {
             ty: Type::new("bool", LlvmTy::I1, 1),
             code: format!("{}", b),
         }),
         NodeKind::Ident(ident) => {
             let var = fn_ctx.env.get(ident.as_ref()).unwrap().clone();
             if var.is_arg {
-                EmitExprRes::Imm(Imm {
+                IrExpr::Imm(Imm {
                     ty: var.ty,
                     code: format!("%{}", var.reg),
                 })
             } else {
                 let reg = fn_ctx.next_reg();
-                EmitExprRes::Reg(Reg {
+                IrExpr::Reg(Reg {
                     ty: var.ty.clone(),
                     code: format!(
                         "%{reg} = load {}, ptr %{}\n",
@@ -287,8 +358,8 @@ fn emit_expr(_ctx: &mut Ctx, fn_ctx: &mut FnCtx, node: NodeRef) -> EmitExprRes {
             let mut code = String::new();
             for node in call.args.iter() {
                 match emit_expr(_ctx, fn_ctx, node.clone()) {
-                    EmitExprRes::Imm(imm) => args += &format!("i32 {}, ", imm.code),
-                    EmitExprRes::Reg(r) => {
+                    IrExpr::Imm(imm) => args += &format!("i32 {}, ", imm.code),
+                    IrExpr::Reg(r) => {
                         code += &r.code;
                         args += &format!("i32 %{}, ", r.reg)
                     }
@@ -300,7 +371,7 @@ fn emit_expr(_ctx: &mut Ctx, fn_ctx: &mut FnCtx, node: NodeRef) -> EmitExprRes {
 
             let reg = fn_ctx.next_reg();
             // TODO fn return type lookup
-            EmitExprRes::Reg(Reg {
+            IrExpr::Reg(Reg {
                 ty: Type::new("u32", LlvmTy::I32, 4),
                 code: format!("{code}%{reg} = call i32 @{}({})\n", call.ident, args),
                 reg,
@@ -322,7 +393,7 @@ fn emit_expr(_ctx: &mut Ctx, fn_ctx: &mut FnCtx, node: NodeRef) -> EmitExprRes {
             let reg = fn_ctx.next_reg();
             // TODO type checking
             match (lhs, rhs) {
-                (EmitExprRes::Imm(lhs), EmitExprRes::Imm(rhs)) => EmitExprRes::Reg(Reg {
+                (IrExpr::Imm(lhs), IrExpr::Imm(rhs)) => IrExpr::Reg(Reg {
                     ty: if ret_bool {
                         Type::new("bool", LlvmTy::I1, 1)
                     } else {
@@ -336,7 +407,7 @@ fn emit_expr(_ctx: &mut Ctx, fn_ctx: &mut FnCtx, node: NodeRef) -> EmitExprRes {
                     ),
                     reg,
                 }),
-                (EmitExprRes::Imm(lhs), EmitExprRes::Reg(rhs)) => EmitExprRes::Reg(Reg {
+                (IrExpr::Imm(lhs), IrExpr::Reg(rhs)) => IrExpr::Reg(Reg {
                     ty: if ret_bool {
                         Type::new("bool", LlvmTy::I1, 1)
                     } else {
@@ -351,7 +422,7 @@ fn emit_expr(_ctx: &mut Ctx, fn_ctx: &mut FnCtx, node: NodeRef) -> EmitExprRes {
                     ),
                     reg,
                 }),
-                (EmitExprRes::Reg(lhs), EmitExprRes::Imm(rhs)) => EmitExprRes::Reg(Reg {
+                (IrExpr::Reg(lhs), IrExpr::Imm(rhs)) => IrExpr::Reg(Reg {
                     ty: if ret_bool {
                         Type::new("bool", LlvmTy::I1, 1)
                     } else {
@@ -366,7 +437,7 @@ fn emit_expr(_ctx: &mut Ctx, fn_ctx: &mut FnCtx, node: NodeRef) -> EmitExprRes {
                     ),
                     reg,
                 }),
-                (EmitExprRes::Reg(lhs), EmitExprRes::Reg(rhs)) => EmitExprRes::Reg(Reg {
+                (IrExpr::Reg(lhs), IrExpr::Reg(rhs)) => IrExpr::Reg(Reg {
                     ty: if ret_bool {
                         Type::new("bool", LlvmTy::I1, 1)
                     } else {
