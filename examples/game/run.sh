7b2074a847edf926d7c746450398317bfc75f027
bsg <cem.saldirim@gmail.com>
2024-07-13 09:24:43 +0300

    schut

--- examples/game/run.sh
+++ examples/game/run.sh
@@ -1,4 +1,4 @@
 rm game *.o
 cargo run --manifest-path ../../Cargo.toml -- ./main.bok --ast
-clang ./main.o -o game -lSDL2 -lSDL2_image
+clang ./main.o -o game -lSDL2 -lSDL2_image -lSDL2_ttf
 ./game
