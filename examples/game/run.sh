ae29a7631096595921b844e612ebf01c98fa8b4a
bsg <cem.saldirim@gmail.com>
2024-11-06 03:39:57 +0300

    remove accidental commit in game run.sh

--- examples/game/run.sh
+++ examples/game/run.sh
@@ -1,5 +1,5 @@
 rm game *.o
-cargo run --manifest-path ../../Cargo.toml -- ./main.bok --asm
+cargo run --manifest-path ../../Cargo.toml -- ./main.bok
 clang ./main.o -o game -lSDL2 -lSDL2_image -lSDL2_ttf -lSDL2_mixer
 echo Running
 ./game
